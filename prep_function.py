d2949ac123274d3d3322d814ab208e88718632a0
Shaked Aglamaz <shakeda@hailo.ai>
2025-07-03 18:55:48 +0300

    final editing

--- prep_function.py
+++ prep_function.py
@@ -1,15 +1,11 @@
 import numpy as np
 import pandas as pd
 import mne
-from scipy import signal
-from mne.preprocessing import annotate_amplitude, ICA
-from itertools import groupby
+from mne.preprocessing import ICA
 
 from prep_config import (
     RAW_CHANNEL_NAMES, CH_NAMES, SFREQ, CH_TYPES,
     MUSE_POSITIONS, BASE_FILTER_SETTINGS,
-    MEDIAN_FILTER_SETTINGS, DYNAMIC_THRESHOLD_SETTINGS,
-    AMPLITUDE_REJECTION_SETTINGS
 )
 
 # === 1. Load EEG data from CSV ===
@@ -51,33 +47,8 @@ def base_filtering(raw):
     raw.filter(None, BASE_FILTER_SETTINGS['lowpass_freq'], fir_design=BASE_FILTER_SETTINGS['filter_design'])
     raw.set_eeg_reference(ref_channels=BASE_FILTER_SETTINGS['eeg_reference'])
 
-# === 3. Median Filter ===
-def median_filter_artifact_removal(raw):
-    if not MEDIAN_FILTER_SETTINGS['enabled']:
-        return raw
 
-    data = raw.get_data().copy()
-    kernel_size = MEDIAN_FILTER_SETTINGS['kernel_size']
-    for i in range(data.shape[0]):
-        signal_clean = signal.medfilt(data[i], kernel_size=kernel_size)
-        artifact_mask = data[i] != signal_clean
-
-        indices = np.where(artifact_mask)[0]
-        for k, g in groupby(enumerate(indices), lambda ix: ix[0] - ix[1]):
-            group = list(g)
-            start = group[0][1]
-            end = group[-1][1]
-            onset = start / raw.info['sfreq']
-            duration = (end - start + 1) / raw.info['sfreq']
-            raw.annotations.append(onset, duration, MEDIAN_FILTER_SETTINGS['annotation_label'])
-
-        data[i] = signal_clean
-
-    raw_clean = raw.copy()
-    raw_clean._data = data
-    return raw_clean
-
-## === 4. Dynamic Thresholding ===
+## === 3. Dynamic Thresholding ===
 def annotate_dynamic_p2p(raw, n_mads=3, win_sec=0.2, step_sec=0.1, label='BAD_dynamic'):
     sf = raw.info['sfreq']
     data = raw.get_data()
@@ -158,17 +129,7 @@ def merge_overlapping_annotations(raw, label='BAD_dynamic'):
     return raw
 
 
-# === 5. Amplitude-Based Rejection ===
-def auto_artifact_rejection(raw):
-    if not AMPLITUDE_REJECTION_SETTINGS['enabled']:
-        return raw
-
-    annotations, _ = annotate_amplitude(raw, peak=AMPLITUDE_REJECTION_SETTINGS['threshold'])
-    for onset, duration, _ in zip(annotations.onset, annotations.duration, annotations.description):
-        raw.annotations.append(onset, duration, AMPLITUDE_REJECTION_SETTINGS['annotation_label'])
-    return raw
-
-# === 6. ICA ===
+# === 4. ICA ===
 def run_ica(raw):
     ica = ICA(n_components=4, method='fastica', random_state=42)
     ica.fit(raw, reject_by_annotation=True)
@@ -185,27 +146,4 @@ def remove_ica_comp_and_plot(ica, raw, comps_to_remove):
     ica.apply(raw_clean)
     raw.plot(n_channels=4, title='Original')
     raw_clean.plot(n_channels=4, title='After ICA cleanup')
-
-
-# === 7. Annotate ICA artifacts ===
-def annotate_ica_artifacts(raw, ica, label='ica_artifact', n_mads=6):
-    sources = ica.get_sources(raw).get_data()
-    sfreq = raw.info['sfreq']
-
-    for i in ica.exclude:
-        component = sources[i]
-        median = np.median(component)
-        mad = np.median(np.abs(component - median))
-        threshold = median + n_mads * mad
-        mask = np.abs(component) > threshold
-
-        indices = np.where(mask)[0]
-        for k, g in groupby(enumerate(indices), lambda ix: ix[0] - ix[1]):
-            group = list(g)
-            start = group[0][1]
-            end = group[-1][1]
-            onset = start / sfreq
-            duration = (end - start + 1) / sfreq
-            raw.annotations.append(onset, duration, label)
-
-    return raw
+    return raw_clean
